de313730fe115fd3d20becf078d2bfe9c14543b1
Phat Future <hungphat0511@gmail.com>
2019-10-03 13:14:32 +0700

    Day 5

---
+++ Day-04/Class vs. Instance.py
@@ -0,0 +1,41 @@
+f  = open('..//Input-Output//input.txt', 'r')
+wr = open('..//Input-Output//output.txt', 'w')
+d = []
+e = []
+g = []
+class Person:
+    def __init__(self,initialAge):
+        self.inittia_age = initialAge
+        if self.inittia_age < 0:
+            print('Age is not valid, setting age to 0')
+
+    def amIOld(self):
+        compte = self.inittia_age
+        if compte>=0 and compte < 13 :
+            print('You are young.')
+        if compte >=13 and compte < 18:
+            print('You are teenager.')
+        if compte >=18:
+            print('You are old.')
+    def yearPasses(self):
+        return self.inittia_age + 3
+
+for line in f:
+    a = int(line.strip())
+    e.append(a)
+    if a < 0:
+        d.append(a)
+g = [x for x in e if x not in d]        #--Remove e from d
+
+for z in d:
+    customer_under0 = Person(z)
+    customer_under0.amIOld()
+    customer_under0_yp = customer_under0.yearPasses()
+    Person(customer_under0_yp).amIOld()
+    print('\n')
+for j in g:
+    customer = Person(j)
+    customer.amIOld()
+    customer_yp = customer.yearPasses()
+    Person(customer_yp).amIOld()
+    print('\n')
